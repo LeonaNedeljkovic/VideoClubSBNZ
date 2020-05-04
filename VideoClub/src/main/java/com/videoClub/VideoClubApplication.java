ed52c55a931fd70e5a527bd330405c4da411f247
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2020-05-04 12:29:31 +0200

    added database conf

---
+++ VideoClub/src/main/java/com/videoClub/VideoClubApplication.java
@@ -0,0 +1,10 @@
+package com.videoClub;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoClubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VideoClubApplication.class, args);
+	}
+}
